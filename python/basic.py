aada1825a06e777a94944a9533ae1275c6addd6a
beenitanep07 <beenitanep07@gmail.com>
2025-06-02 17:26:54 +0545

    python basic

---
+++ python/basic.py
@@ -0,0 +1,20 @@
+# basic python printing statement
+print('Hello World!')
+
+# variable : temporary container to store value
+# declaring and initializing variable
+a = 10
+b = 20
+# basic python addition statement
+sum = a + b
+print("The sum of 10 and 20 is",sum) # concatination: adding two types of data together
+print("The sum of", a , "and" ,  b , "is" ,sum )
+
+# declaring and initializing string variable
+string1 = "This is test string"
+string2 = " added"
+print(string1)
+
+
+sum1 = string1 + string2
+print(sum1)
---
+++ python/operators.py
@@ -0,0 +1,8 @@
+# arithmetic operator 
+# + , - , * , / , // , %
+num1 = 11
+num2 = 3 
+div = num1 // num2 
+mod = num1 % num2 #  remainder dincha
+print("The division is", div)
+print("The modulus is", mod)
