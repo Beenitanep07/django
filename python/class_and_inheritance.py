39cd3f4bea203781ba983cbb0753ef584a135245
beenitanep07 <beenitanep07@gmail.com>
2025-06-04 15:01:41 +0545

    oop simple calculator added

--- python/class_and_inheritance.py
+++ python/class_and_inheritance.py
@@ -8,6 +8,15 @@ class B(A): #inheritance #single level inheritance
 class C(B): #multi level inheritance
     def print_data():
         print("This is class c")
+        
+class Sum: #construtor
+    def __init__(self,num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    
+    def add(self):
+        result = self.num1 + self.num2
+        return result
 
 # obj = A #intance call gareko i.e. tyo class ko copy banako 
 # obj.display_name()
@@ -16,7 +25,11 @@ class C(B): #multi level inheritance
 # obj2.print_name()
 # obj2.display_name() #this can be done due to inheritance
 
-obj3 = C
-obj3.print_data()
-obj3.display_name()
-obj3.print_name()
+# obj3 = C
+# obj3.print_data()
+# obj3.display_name()
+# obj3.print_name()
+
+#due to construtor
+obj4 = Sum(10,20)
+print(obj4.add())
